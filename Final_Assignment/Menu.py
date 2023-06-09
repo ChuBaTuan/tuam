5fca18d7ee6527e81aaf82c1d4f8f8a55678dc2f
ChuBaTuan <113301726+ChuBaTuan@users.noreply.github.com>
2023-06-09 21:16:06 +0700

    Add files via upload

---
+++ Final_Assignment/Menu.py
@@ -0,0 +1,15 @@
+
+class Menu():
+    """
+    Menu chương trình
+    """
+    def menu(self):
+        print("===== Quản lý sách =====")
+        print("1. Xem danh mục sách")
+        print("2. Thêm sách mới")
+        print("3. Cập nhật thông tin sách")
+        print("4. Xóa thông tin sách")
+        print("5. Tìm kiếm sách")
+        print("6. Sắp xếp sách")
+        print("0. Thoát")
+        print("============================")
